f8dab48bb1a89eecede1ffbcfd2f5f5d7b0668a1
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 19:21:17 -0300

    admin and user middleware class

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user()->role == 'admin') {
+            return $next($request);
+        } else {
+            request()->session()->flash('error', 'Você não tem permissão para acessar essa página.');
+            return redirect()->route($request->user()->role);
+        }
+    }
+}
