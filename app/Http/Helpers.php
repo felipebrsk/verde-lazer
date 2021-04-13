dbf0a113de09a753b4b03e5113d7509b0906ae5a
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 09:09:47 -0300

    Message list to Helpers class

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Message;
+
 class Helper
 {
-    
+    public static function messageList()
+    {
+        return Message::whereNull('read_at')->orderBy('created_at', 'desc')->get();
+    }
 }
 ?>
