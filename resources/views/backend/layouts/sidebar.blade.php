c05056f1f98ab53d5b119f660ab023c171b1be9c
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 19:36:21 -0300

    siderbar structure

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,11 +1,231 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    {{-- Sidebar - Admin --}}
-    <a href="{{ route('admin') }}" class="sidebar-brand d-flex align-items-center justify-content-center">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin') }}">
         <div class="sidebar-brand-icon">
             <i class="fas fa-user-cog"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Admin</div>
     </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{ route('admin') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Mídia
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Gerenciador de arquivos</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-image"></i>
+            <span>Banners</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de banners:</h6>
+                <a class="collapse-item" href="#">Banners</a>
+                <a class="collapse-item" href="#">Adicionar banner</a>
+            </div>
+        </div>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Loja
+    </div>
+
+    <!-- Categories -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#categoryCollapse"
+            aria-expanded="true" aria-controls="categoryCollapse">
+            <i class="fas fa-sitemap"></i>
+            <span>Categoria</span>
+        </a>
+        <div id="categoryCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de categorias:</h6>
+                <a class="collapse-item" href="#">Categorias</a>
+                <a class="collapse-item" href="#">Adicionar categoria</a>
+            </div>
+        </div>
+    </li>
+    {{-- Products --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#productCollapse"
+            aria-expanded="true" aria-controls="productCollapse">
+            <i class="fas fa-cubes"></i>
+            <span>Produtos</span>
+        </a>
+        <div id="productCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de produtos:</h6>
+                <a class="collapse-item" href="#">Produtos</a>
+                <a class="collapse-item" href="#">Adicionar produto</a>
+            </div>
+        </div>
+    </li>
+
+    {{-- Brands --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#brandCollapse" aria-expanded="true"
+            aria-controls="brandCollapse">
+            <i class="fas fa-table"></i>
+            <span>Marcas</span>
+        </a>
+        <div id="brandCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de marcas:</h6>
+                <a class="collapse-item" href="#">Marcas</a>
+                <a class="collapse-item" href="#">Adicionar marca</a>
+            </div>
+        </div>
+    </li>
+
+    {{-- Shipping --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#shippingCollapse"
+            aria-expanded="true" aria-controls="shippingCollapse">
+            <i class="fas fa-truck"></i>
+            <span>Transporte</span>
+        </a>
+        <div id="shippingCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de transporte:</h6>
+                <a class="collapse-item" href="#">Transportes</a>
+                <a class="collapse-item" href="#">Adicionar transporte</a>
+            </div>
+        </div>
+    </li>
+
+    <!--Orders -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-hammer fa-chart-area"></i>
+            <span>Locações</span>
+        </a>
+    </li>
+
+    <!-- Reviews -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-comments"></i>
+            <span>Reviews</span></a>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Publicações
+    </div>
+
+    <!-- Posts -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#postCollapse" aria-expanded="true"
+            aria-controls="postCollapse">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Publicações</span>
+        </a>
+        <div id="postCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de publicações</h6>
+                <a class="collapse-item" href="#">Publicações</a>
+                <a class="collapse-item" href="#">Adicionar publicação</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Category -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#postCategoryCollapse"
+            aria-expanded="true" aria-controls="postCategoryCollapse">
+            <i class="fas fa-sitemap fa-folder"></i>
+            <span>Categorias</span>
+        </a>
+        <div id="postCategoryCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de categoria:</h6>
+                <a class="collapse-item" href="#">Categorias</a>
+                <a class="collapse-item" href="#">Adicionar categoria</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Tags -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#tagCollapse" aria-expanded="true"
+            aria-controls="tagCollapse">
+            <i class="fas fa-tags fa-folder"></i>
+            <span>Marcações</span>
+        </a>
+        <div id="tagCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Opções de marcações:</h6>
+                <a class="collapse-item" href="#">Marcações</a>
+                <a class="collapse-item" href="#">Adicionar marcação</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Comments -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-comments fa-chart-area"></i>
+            <span>Comentários</span>
+        </a>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Configurações gerais
+    </div>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-table"></i>
+            <span>Cupons</span></a>
+    </li>
+    <!-- Users -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-users"></i>
+            <span>Usuários</span></a>
+    </li>
+    <!-- General settings -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-cog"></i>
+            <span>Configurações</span></a>
+    </li>
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
 </ul>
