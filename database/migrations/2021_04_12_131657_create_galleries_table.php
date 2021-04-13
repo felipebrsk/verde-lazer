42939facfaa3d2c1e025a21ed013bcea65004dcf
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 09:05:26 -0300

    Create 2021_04_12_131657_create_galleries_table.php

---
+++ database/migrations/2021_04_12_131657_create_galleries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            
+            $table->unsignedBigInteger('product_id')->nullable();
+            
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
