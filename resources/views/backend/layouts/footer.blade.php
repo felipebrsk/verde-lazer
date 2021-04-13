07daa8df650fdd8b22e500c620c3defd21a9a360
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 19:33:47 -0300

    backend footer n header fix

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-{{-- Footer --}}
+<!-- Footer -->
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
@@ -7,8 +7,66 @@
         </div>
     </div>
 </footer>
+<!-- End of Footer -->
 
+</div>
+<!-- End of Content Wrapper -->
 
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tem certeza que deseja sair?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Selecione "Sair" se está certo de que quer encerrar a sua sessão.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                <a class="btn btn-primary" href="login.html">Sair</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-{{-- End of Footer --}}
+<script src="{{ asset('backend/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"
+    integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ=="
+    crossorigin="anonymous"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/4.1.3/js/sb-admin-2.min.js"
+    integrity="sha512-COtY6/Rv4GyQdDShOyay/0YI4ePJ7QeKwtJIOCQ3RNE32WOPI4IYxq6Iz5JWcQpnylt/20KBvqEROZTEj/Hopw=="
+    crossorigin="anonymous"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('backend/js/Chart.min.js') }}"></script>
+
+<!-- Page level custom scripts -->
+{{-- <script src="{{asset('backend/js/chart-area-demo.js')}}"></script> --}}
+{{-- <script src="{{asset('backend/js/chart-pie-demo.js')}}"></script> --}}
+
+@stack('scripts')
+
+<script>
+    setTimeout(function() {
+        $('.alert').slideUp();
+    }, 4000);
+
+</script>
