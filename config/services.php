016ab1d67403384b226209f595255b46474ee67f
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-13 13:41:43 -0300

    socialite services ids

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('APP_URL') . '/login/github/callback',
     ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/google/callback',
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/facebook/callback',
+    ],
 ];
