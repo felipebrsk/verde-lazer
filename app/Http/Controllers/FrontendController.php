6de7da2b48e9edc522597699f1f580a6d1a9c148
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-04-14 10:02:59 -0300

    login method - put user session

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -36,6 +36,26 @@ class FrontendController extends Controller
     }
 
     /**
+     *  Submit the login form.
+     * 
+     *  @param \Illuminate\Http\Request $request
+     *  @return \Illuminate\Http\Response
+     */
+    public function loginSubmit(Request $request)
+    {
+        $data = $request->all();
+
+        if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'status' => 'active'])) {
+            Session::put('user', $data['email']);
+            request()->session()->flash('success', 'Login efetuado com sucesso!');
+            return redirect()->route('home');
+        } else {
+            request()->session()->flash('error', 'E-mail ou senha inválidos. Por favor, tente novamente!');
+            return redirect()->back();
+        }
+    }
+
+    /**
      *  Function to register the user in database.
      * 
      */
